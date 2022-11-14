6f412994b9abfc194652f8a628e407ea3729aed3
rafa <gomezra72.a@gmail.com>
2022-11-14 12:39:07 +0000

    14/11/2022 morning

---
+++ javascript_files/display_menu.js
@@ -0,0 +1,16 @@
+function myFunctionMenu() {
+  document.getElementById("my_menu").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.menu-button')) {
+    var dropdowns = document.getElementsByClassName("menu-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
