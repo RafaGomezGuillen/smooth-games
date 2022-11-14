20d7324a3c4e045bbee41e7754ee7f4076464300
Rafa Gómez <gomezra72.a@gmail.com>
2022-11-14 12:44:30 +0000

    Add files via upload

---
+++ javascript_files/display_statistics.js
@@ -0,0 +1,8 @@
+function myFunctionStatistics() {
+  var x = document.getElementById("statisticsContent");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
