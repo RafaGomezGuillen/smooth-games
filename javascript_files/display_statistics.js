dd808f3a2df053e844e5a1ac4d64db44da2440b7
Rafa Gómez <gomezra72.a@gmail.com>
2022-11-14 19:26:55 +0000

    Delete display_statistics.js

--- javascript_files/display_statistics.js
+++
@@ -1,8 +0,0 @@
-function myFunctionStatistics() {
-  var x = document.getElementById("statisticsContent");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
