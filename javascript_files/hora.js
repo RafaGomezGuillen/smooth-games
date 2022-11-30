20110384936635b1004d884e2b17d4ca6bbdb58b
Rafa Gómez <gomezra72.a@gmail.com>
2022-11-30 17:16:03 +0000

    Update hora.js

--- javascript_files/hora.js
+++ javascript_files/hora.js
@@ -1,28 +1,31 @@
 if (localStorage.getItem("count_timer")) {
   var count_timer = localStorage.getItem("count_timer");
 } else {
-  var count_timer = 1800;
+  var count_timer = 10;
 }
+
 var minutes = parseInt(count_timer / 60);
-var seconds = parseInt(count_timer % 60);
+var seconds_timer = parseInt(count_timer % 60);
+
 function countDownTimer() {
-  if (seconds < 10) {
-    seconds = "0" + seconds;
+  if (seconds_timer < 10) {
+    seconds_timer = "0" + seconds_timer;
   }
   if (minutes < 10) {
     minutes = "0" + minutes;
   }
 
   document.getElementById("minute").innerText = minutes;
-  document.getElementById("second").innerText = seconds;
+  document.getElementById("second").innerText = seconds_timer;
   if (count_timer <= 0) {
     localStorage.clear("count_timer");
   } else {
     count_timer = count_timer - 1;
     minutes = parseInt(count_timer / 60);
-    seconds = parseInt(count_timer % 60);
+    seconds_timer = parseInt(count_timer % 60);
     localStorage.setItem("count_timer", count_timer);
     setTimeout("countDownTimer()", 1000);
   }
 }
+
 setTimeout("countDownTimer()", 1000);
